4d81e180eb27e983686536ab698e4c0b2971a09c
priyankasagar18 <118506928+priyankasagar18@users.noreply.github.com>
2025-01-02 18:07:58 +0530

    Add files via upload

---
+++ OutDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var numbers = new[] { 5, -7, 10, -16, 12 };
+foreach (var number in numbers)
+{
+    Console.WriteLine(number);
+}
+
+
+var positvieNumbers = GetOnlyPositive(numbers, out int NonpositiveCount);
+//foreach (var positiveNumber in positvieNumbers)
+//{
+//    Console.WriteLine("positive number is " +positiveNumber);
+//}
+Console.WriteLine(NonpositiveCount);
+
+List<int> GetOnlyPositive(int[] numbers, out int countNonPositive)
+{
+    countNonPositive = 0;
+    var result = new List<int>();
+    foreach (var number in numbers)
+    {
+        if(number>0) result.Add(number);
+        else
+        {
+            countNonPositive++;
+        }
+    }
+    return result;
+}
+
+
